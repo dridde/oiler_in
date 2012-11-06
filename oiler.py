ed5a73c57633eb19d7dbca36b1e47f611ac1b336
Matthias Bauer <moeffju@gmail.com>
2012-11-06 22:51:18 +0100

    UTF-8 in Twitter Mentions

--- oiler.py
+++ oiler.py
@@ -1,7 +1,37 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
-import sys, socket, string, time, datetime, urllib, re, random, tweepy, threading, config
+import config
+
+import sys, socket, string, time, datetime, urllib, re, random, tweepy, threading, htmlentitydefs
+
+##
+# Removes HTML or XML character references and entities from a text string.
+#
+# @source http://effbot.org/zone/re-sub.htm#unescape-html
+# @param text The HTML (or XML) source text.
+# @return The plain text, as a Unicode string, if necessary.
+
+def unescape(text):
+	def fixup(m):
+		text = m.group(0)
+		if text[:2] == "&#":
+			# character reference
+			try:
+				if text[:3] == "&#x":
+					return unichr(int(text[3:-1], 16))
+				else:
+					return unichr(int(text[2:-1]))
+			except ValueError:
+				pass
+		else:
+			# named entity
+			try:
+				text = unichr(htmlentitydefs.name2codepoint[text[1:-1]])
+			except KeyError:
+				pass
+		return text # leave as is
+	return re.sub("&#?\w+;", fixup, text)
 
 # MetaKram
 
@@ -433,7 +463,7 @@ def twitterLurk():
 		mentions = api.mentions();
 		for status in mentions:
 			if (status.created_at > datetime.datetime.utcnow()-datetime.timedelta(minutes=1)):
-				irc.send("PRIVMSG " + chan + " :" + "Tweet von " + str("@" + status.author.screen_name) + ": " + status.text.encode('utf-8') + "\r\n")
+				irc.send("PRIVMSG " + chan + " :" + "Tweet von " + str("@" + status.author.screen_name) + ": " + unescape(status.text).encode('utf-8') + "\r\n")
 
 t = threading.Thread(target=twitterLurk)
 t.daemon = True
