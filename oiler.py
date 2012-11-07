0df65be5107b1a322b4680d133b298e1c93b7ef2
Matthias Bauer <moeffju@gmail.com>
2012-11-07 16:15:02 +0100

    Adapt for new func signatures

--- oiler.py
+++ oiler.py
@@ -6,17 +6,22 @@ import config
 from oilib.helpers import *
 from oilib.connection import IRCConnection
 
-def handle_privmsg(target, message):
-	print 'PRIVMSG %s: %s' % (target, message)
+def handle_privmsg(irc, nick, userhost, target, message):
+	print '<%s@%s/%s> %s' % (nick, userhost, target, message)
 	return True
 
-def handle_unknown(prefix, command, args):
+def handle_kick(irc, nick, userhost, target, victim):
+	irc.send('JOIN', target)
+	return True
+
+def handle_unknown(irc, prefix, command, args):
 	print 'UNKOWN: %s %s %s' % (prefix, command, args)
 	return False
 
 def main():
 	irc = IRCConnection(server=config.server, port=config.port, password=config.password, nick=config.nick, realname=config.realname, user=config.user, channels=[config.chan])
 	irc.on('privmsg', handle_privmsg)
+	irc.on('kick', handle_kick)
 	irc.on('*', handle_unknown)
 	irc.connect()
 
